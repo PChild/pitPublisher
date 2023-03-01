e66b469152ab20752e9a7a6a9c951ec301b0fb2a
pchild <childrep@vt.edu>
2023-02-28 19:13:09 -0500

    Two line initial stuff

--- pitPublisher.py
+++ pitPublisher.py
@@ -2,6 +2,8 @@ from fabric import Connection
 from threading import Thread
 from colorama import Fore
 from types import SimpleNamespace
+from string import Template
+from io import StringIO
 import tbapy
 import time
 import json
@@ -9,31 +11,90 @@ import json
 settings = json.load( open('settings.json') )
 tba = tbapy.TBA(settings['key'])
 is_sim = settings['is_sim']
+use_simple = settings['use_simple']
 
-displayed_match = ''
+displayed_match = 'NM-1'
+pred_time = '00:00'
 red_teams = []
 blue_teams = []
+team_names = {}
+current_match = 'NM-2'
 
-# basic function to update an individual sign with specified text and color. 
-def update_sign(conn, text):
-    if is_sim:
-        conn = SimpleNamespace(host=conn)
+team_sign_text = Template('''
+llm_builder.llm new_msg 0,0,16,96 "normal"
+llm_builder.llm add_region 0,0,8,96 "1" "appear" "appear" "fastest" "16000" "left" "middle"
+llm_builder.llm add_text 0,0,8,96 "1" "8" "normal" "block" "normal" "$color" "black" "none" "none" "--" "$pos:$team"
+llm_builder.llm add_region 8,0,8,96 "1" "appear" "appear" "fastest" "16000" "left" "bottom"   
+llm_builder.llm add_text 8,0,8,96 "1" "8" "normal" "block" "normal" "$color" "black" "none" "none" "--" "$name"
+''')
 
-    # use the hostname to choose color to apply.
+match_sign_text = Template('''
+llm_builder.llm new_msg 0,0,16,96 "normal"
+llm_builder.llm add_region 0,0,8,96 "1" "appear" "appear" "fastest" "16000" "left" "middle"
+llm_builder.llm add_text 0,0,8,96 "1" "8" "normal" "block" "normal" "$color" "black" "none" "none" "--" "Now Playing: $curr"
+llm_builder.llm add_region 8,0,8,96 "1" "appear" "appear" "fastest" "16000" "left" "bottom"   
+llm_builder.llm add_text 8,0,8,96 "1" "8" "normal" "block" "normal" "$color" "black" "none" "none" "--" "Next Match:  $next"
+''')
+
+
+
+time_sign_text = Template('''
+                          
+''')
+
+
+def get_color(conn):
     color = Fore.YELLOW if is_sim else 'yellow'
     if 'red' in conn.host:
         color = Fore.RED if is_sim else 'red'
     elif 'blue' in conn.host:
-        color = Fore.GREEN if is_sim else 'green'
-        
-    text_val = str(text).upper()
+        color = Fore.GREEN if is_sim else 'green' 
+    return color
+
+def update_2l_sign(conn, text):
+    color = get_color(conn)
     
-    if is_sim:
-        print(color + str(text_val) + Fore.WHITE)
+    if 'red' in conn.host or 'blue' in conn.host:
+        team_name = team_names[text]
+        team_val = ' ' * (7 - len(text)) + text
+        
+        if is_sim:
+            print(color + "%s:%s" % (conn.host[-1], team_val) + '\n' + team_name + Fore.WHITE)
+        
+        else:
+            send_file = StringIO(team_sign_text.substitute(pos=conn.host[-1], team=team_val, name=team_name, color=color))
+            conn.put(send_file, 'test.llm')
+            conn.close()
+            
+    
+    if 'info1' in conn.host:
+        if is_sim:
+            print(color + "Now Playing: " + current_match + "\nNext Match:  " + displayed_match + Fore.WHITE)
+        
+        else:
+            send_file = StringIO(match_sign_text.substitute(curr=current_match, next=displayed_match, color=color))
+            conn.put(send_file, 'test.llm')
+            conn.close()
+    
+    if 'spare1' in conn.host:
+        if is_sim:
+            print(color + "Scheduled: " + text +"\nPredicted: " + pred_time + Fore.WHITE)
+        
+
+# basic function to update an individual sign with specified text and color. 
+def update_sign(conn, text):
+    if use_simple:
+        color = get_color(conn)
+        text_val = str(text).upper()
+        
+        if is_sim:
+            print(color + str(text_val) + Fore.WHITE)
+        else:
+            conn.run("localmsgs delete -f ALL")
+            conn.run("localmsgs compose -c %s -d 0 -f test.llm -p appear -s 16 -t '%s'" % (color, text_val))
+            conn.close()
     else:
-        conn.run("localmsgs delete -f ALL")
-        conn.run("localmsgs compose -c %s -d 0 -f test.llm -p appear -s 16 -t '%s'" % (color, text_val))
-        conn.close()
+        update_2l_sign(conn, str(text).upper())
     
 # takes in red and blue teams, and basic match info to update all displays in parallel
 def update_displays(red_teams, blue_teams, match_num, match_time):
@@ -44,7 +105,7 @@ def update_displays(red_teams, blue_teams, match_num, match_time):
     
     threads = []
     for idx, conn in enumerate(connections):
-        conn = conn['name'] if is_sim else conn #use sign name as connection if sim
+        conn = SimpleNamespace(host=conn['name']) if is_sim else conn #use sign name as connection if sim
         t = Thread(target=update_sign, args=(conn, sign_data[idx]))
         threads.append(t)
         t.start()
@@ -65,9 +126,13 @@ def get_next_match(team, event):
 # remove leading 'FRC' from team keys, prepend _ to pad text, account for 1 to 4 digit teams with a padder
 def format_team_keys(team_keys):
     fixed_list = []
-    for idx, t in enumerate(team_keys[:3]):
-        padder = ' ' * (7 - len(t))
-        fixed_list.append('_' + str(idx + 1) + ': ' + padder + t[3:])
+    if use_simple:
+        for idx, t in enumerate(team_keys[:3]):
+            padder = ' ' * (7 - len(t))
+            fixed_list.append('_' + str(idx + 1) + ': ' + padder + t[3:])
+    else:
+        for idx, t in enumerate(team_keys[:3]):
+            fixed_list.append(t[3:])
     return fixed_list        
 
 # Check if the signs need to be updated by comparing match #, team #s
@@ -86,10 +151,19 @@ def check_match_status():
         
         match_time = time.strftime('%I:%M %p', time.localtime(next_match['time']))
         
+        print('Updating screens to match', displayed_match)
         update_displays(red_teams, blue_teams, displayed_match, match_time)
         
+
+def get_team_names():
+    global team_names
+    for team in tba.event_teams(settings['event']):
+        team_names[str(team['team_number'])] = team['nickname'][:settings['name_lim']]
+
 # Main function to run code.  
 def main():
+    get_team_names()
+    
     while True:
         check_match_status()
         time.sleep(settings['delay'])
