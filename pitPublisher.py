57d382c38571731b105f41b04bbc4269aa612da7
pchild <childrep@vt.edu>
2023-03-13 15:39:25 -0400

    Added code for next match sign, tested in sim

--- pitPublisher.py
+++ pitPublisher.py
@@ -3,7 +3,6 @@ from threading import Thread
 from colorama import Fore
 from types import SimpleNamespace
 from string import Template
-from io import StringIO
 import tbapy
 import time
 import json
@@ -50,6 +49,7 @@ llm_builder.llm add_text 9,57,7,39 "1" "6" "normal" "block" "condensed" "yellow"
 ''')
 
 
+# Sets up colors based on sign type for both real hardware and sim
 def get_color(conn):
     color = Fore.YELLOW if is_sim else 'yellow'
     if 'red' in conn.host:
@@ -58,51 +58,45 @@ def get_color(conn):
         color = Fore.GREEN if is_sim else 'green' 
     return color
 
+# Helper function to run repetitive connection tasks for updating signs.
+def run_2l_update(conn, file_build_str):
+    conn.run("rm -f /tmp/test")
+    conn.run(file_build_str)
+    conn.run("localmsgs delete -f ALL")
+    conn.run('localmsgs compose -e /tmp/test -f mymsg.llm')
+    conn.close()
+
+# Logic function that deals with two line sign crap.  
 def update_2l_sign(conn, text):
     color = get_color(conn)
     
     if 'red' in conn.host or 'blue' in conn.host:
         team_name = '\\"' + team_names[text] + '\\"'
         team_val = '\\"' + ' ' * (7 - len(text)) + text + '\\"'
-        print(team_val)
         
         if is_sim:
             print(color + "%s:%s" % (conn.host[-1], team_val) + '\n' + team_name + Fore.WHITE)
         
         else:
-            conn.run("rm -f /tmp/test")
             file_build_str = 'echo -e "' + team_sign_text.substitute(pos=conn.host[-1], team=team_val, name=team_name, color=color) + '" >> /tmp/test'
-            conn.run(file_build_str)
-            conn.run("localmsgs delete -f ALL")
-            conn.run('localmsgs compose -e /tmp/test -f mymsg.llm')
-            conn.close()
+            run_2l_update(conn, file_build_str)
             
-    
     if 'info1' in conn.host:
         if is_sim:
-            print(color + "Now Playing: " + current_match + "\nNext Match:  " + displayed_match + Fore.WHITE)
+            print(color + "Now Playing: " + current_match + "\nNext Match:  " + text + Fore.WHITE)
         
         else:
-            #conn.run("rm -f /tmp/test")
-            print(text)
-            #file_build_str = 'echo -e "' + match_sign_text.substitute(curr=current_match, next=next_match) + '" >> /tmp/test'  #TODO implement current and our next match numbers
-            #conn.run(file_build_str)
-            #conn.run("localmsgs delete -f ALL")
-            #conn.run('localmsgs compose -e /tmp/test -f mymsg.llm')
-            #conn.close()
+            file_build_str = 'echo -e "' + match_sign_text.substitute(curr=current_match, next=text) + '" >> /tmp/test'
+            run_2l_update(conn, file_build_str)
     
     if 'info2' in conn.host:
         if is_sim:
-            print("CURRENT TIME:", text)
+            print(color + "Predicted:", text)
+            print(color + "Current:", time.strftime("%I:%M"))
         
         else:
-            conn.run("rm -f /tmp/test")
             file_build_str = 'echo -e "' + time_sign_text.substitute(matchtime=text) + '" >> /tmp/test' #TODO fix next match time
-            conn.run(file_build_str)
-            conn.run("localmsgs delete -f ALL")
-            conn.run('localmsgs compose -e /tmp/test -f mymsg.llm')
-            conn.close()
-        
+            run_2l_update(conn, file_build_str)
 
 # basic function to update an individual sign with specified text and color. 
 def update_sign(conn, text):
@@ -128,10 +122,11 @@ def update_displays(red_teams, blue_teams, match_num, match_time):
     
     threads = []
     for idx, conn in enumerate(connections):
-        conn = SimpleNamespace(host=conn['name']) if is_sim else conn #use sign name as connection if sim
-        t = Thread(target=update_sign, args=(conn, sign_data[idx]))
-        threads.append(t)
-        t.start()
+        conn = SimpleNamespace(host=conn['name']) if is_sim else conn # use sign name as connection if sim
+        if conn.host != 'info1': # handle info1 separately as it updates with the field, not us.
+            t = Thread(target=update_sign, args=(conn, sign_data[idx]))
+            threads.append(t)
+            t.start()
     
     for t in threads:
         t.join()
@@ -156,28 +151,60 @@ def format_team_keys(team_keys):
     else:
         for idx, t in enumerate(team_keys[:3]):
             fixed_list.append(t[3:])
-    return fixed_list        
+    return fixed_list
 
-# Check if the signs need to be updated by comparing match #, team #s
+# Finds the earliest match in the schedule that doesn't have a result yet.
+def get_current_event_match(event):
+    team_matches = sorted(tba.event_matches(event), key=lambda match: match['time'])
+    next_match = team_matches[-1]
+    for match in team_matches:
+        if match['post_result_time'] is None:
+            next_match = match
+            break
+    return next_match    
+
+# Helper function to update just the next match sign in its own thread.
+def update_now_playing():
+    sign = settings['signs'][0]
+    conn = sign if is_sim else Connection('inova@'+sign['name'], connect_kwargs={'password':sign['pass']})
+    conn = SimpleNamespace(host=conn['name']) if is_sim else conn #use sign name as connection if sim
+    t = Thread(target=update_sign, args=(conn, displayed_match))
+    t.start()
+    t.join()
+
+# Check if the signs need to be updated by comparing match #, team #s, also logic for now playing
 def check_match_status():   
-    global displayed_match, red_teams, blue_teams
+    global displayed_match, red_teams, blue_teams, current_match
     next_match = get_next_match(settings['team'], settings['event'])
-    
     new_match = next_match['comp_level'].upper() + str(next_match['match_number'])!= displayed_match
+    if new_match:
+        displayed_match = next_match['comp_level'].upper() + str(next_match['match_number'])
+    
+    new_curr = get_current_event_match(settings['event'])
+    now_playing = new_curr['comp_level'].upper() + str(new_curr['match_number'])
+    if now_playing != current_match:
+        current_match = now_playing        
+        update_now_playing()
+    
+    
     new_red_teams = format_team_keys(next_match['alliances']['red']['team_keys']) != red_teams
     new_blue_teams = format_team_keys(next_match['alliances']['blue']['team_keys']) != blue_teams
     
     if new_match or new_red_teams or new_blue_teams:
         red_teams = format_team_keys(next_match['alliances']['red']['team_keys'])
         blue_teams = format_team_keys(next_match['alliances']['blue']['team_keys'])
-        displayed_match = next_match['comp_level'].upper() + str(next_match['match_number'])
         
-        match_time = time.strftime('%_I:%M', time.localtime(next_match['predicted_time']))
+        
+        try:
+            match_time = time.strftime('%_I:%M', time.localtime(next_match['predicted_time']))
+        except:
+            # fuck you microsoft
+             match_time = time.strftime('%#I:%M', time.localtime(next_match['predicted_time']))
         
         print('Updating screens to match', displayed_match)
         update_displays(red_teams, blue_teams, displayed_match, match_time)
         
-
+# simple func to grab team names when the script starts
 def get_team_names():
     global team_names
     for team in tba.event_teams(settings['event']):
